b3c3f6eca875903ab0fefa0bdd1eb9f78008aca1
Ali Beyaz <symbuzzer@users.noreply.github.com>
2024-12-10 13:51:07 +0300

    Update and rename tool_SAVEOVERLAY.sh to saveoverlay.sh

--- saveoverlay.sh
+++ saveoverlay.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-version="1.0.1"
+version="1.0.2"
 
 script_dir="$(dirname "$(realpath "$0")")"
 tool_dir="$script_dir/knulli_tools"
 
 mkdir -p "$tool_dir"
 
-curl -L https://raw.githubusercontent.com/symbuzzer/knulli_tools/refs/heads/main/tool_SAVEOVERLAY.sh -o "$script_dir/tool_SAVEOVERLAY.sh"
+curl -L https://raw.githubusercontent.com/symbuzzer/knulli_tools/refs/heads/main/saveoverlay.sh -o "$script_dir/saveoverlay.sh"
 curl -L https://raw.githubusercontent.com/symbuzzer/knulli_tools/refs/heads/main/pythonscripts/saveoverlay.pygame -o "$tool_dir/saveoverlay.pygame"
 
-#if [ ! -f "$tool_dir/tool_SAVEOVERLAY.pygame" ]; then
+#if [ ! -f "$tool_dir/saveoverlay.pygame" ]; then
 #    curl -L https://raw.githubusercontent.com/symbuzzer/knulli_tools/refs/heads/main/pythonscripts/saveoverlay.pygame -o "$tool_dir/saveoverlay.pygame"
 #fi
 
