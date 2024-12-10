f317791390452cb4a0e5bd6d38851a75ff60353d
Ali Beyaz <symbuzzer@users.noreply.github.com>
2024-12-10 13:02:14 +0300

    Update tool_SAVEOVERLAY.sh

--- tool_SAVEOVERLAY.sh
+++ tool_SAVEOVERLAY.sh
@@ -7,10 +7,10 @@ tool_dir="$script_dir/knulli_tools"
 
 mkdir -p "$tool_dir"
 
-curl -L https://raw.githubusercontent.com/symbuzzer/knulli-tools/refs/heads/main/tool_SAVEOVERLAY.sh -o "$script_dir/tool_SAVEOVERLAY.sh"
+curl -L https://raw.githubusercontent.com/symbuzzer/knulli_tools/refs/heads/main/tool_SAVEOVERLAY.sh -o "$script_dir/tool_SAVEOVERLAY.sh"
 
 if [ ! -f "$tool_dir/tool_SAVEOVERLAY.pygame" ]; then
-    curl -L https://raw.githubusercontent.com/symbuzzer/knulli-tools/refs/heads/main/pythonscripts/tool_SAVEOVERLAY.pygame -o "$tool_dir/tool_SAVEOVERLAY.pygame"
+    curl -L https://raw.githubusercontent.com/symbuzzer/knulli_tools/refs/heads/main/pythonscripts/tool_SAVEOVERLAY.pygame -o "$tool_dir/tool_SAVEOVERLAY.pygame"
 fi
 
 if command -v python3 &>/dev/null; then
