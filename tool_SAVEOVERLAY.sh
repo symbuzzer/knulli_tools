4e57ab4da1da38c43c96f5d193c531703365277a
Ali Beyaz <symbuzzer@users.noreply.github.com>
2024-12-10 13:47:27 +0300

    Update tool_SAVEOVERLAY.sh

--- tool_SAVEOVERLAY.sh
+++ tool_SAVEOVERLAY.sh
@@ -1,24 +1,23 @@
 #!/bin/bash
 
-version="1.0.0"
+version="1.0.1"
 
 script_dir="$(dirname "$(realpath "$0")")"
 tool_dir="$script_dir/knulli_tools"
 
 mkdir -p "$tool_dir"
-chmod -R u+w "$tool_dir"
 
 curl -L https://raw.githubusercontent.com/symbuzzer/knulli_tools/refs/heads/main/tool_SAVEOVERLAY.sh -o "$script_dir/tool_SAVEOVERLAY.sh"
-curl -L https://raw.githubusercontent.com/symbuzzer/knulli_tools/refs/heads/main/pythonscripts/tool_SAVEOVERLAY.pygame -o "$tool_dir/tool_SAVEOVERLAY.pygame"
+curl -L https://raw.githubusercontent.com/symbuzzer/knulli_tools/refs/heads/main/pythonscripts/saveoverlay.pygame -o "$tool_dir/saveoverlay.pygame"
 
 #if [ ! -f "$tool_dir/tool_SAVEOVERLAY.pygame" ]; then
-#    curl -L https://raw.githubusercontent.com/symbuzzer/knulli_tools/refs/heads/main/pythonscripts/tool_SAVEOVERLAY.pygame -o "$tool_dir/tool_SAVEOVERLAY.pygame"
+#    curl -L https://raw.githubusercontent.com/symbuzzer/knulli_tools/refs/heads/main/pythonscripts/saveoverlay.pygame -o "$tool_dir/saveoverlay.pygame"
 #fi
 
 if command -v python3 &>/dev/null; then
-    python3 "$tool_SAVEOVERLAY.pygame"
+    python3 "$tool_dir/saveoverlay.pygame"
 elif command -v python &>/dev/null; then
-    python "$tool_SAVEOVERLAY.pygame"
+    python "$tool_dir/saveoverlay.pygame"
 else
     exit 1
 fi
